691ce2ca78dfe3844fa9fa8921d653454d3a8a23
bear1099 <ill.bear.1099@gmail.com>
2021-01-20 17:40:03 +0900

    release

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,4 +1,7 @@
 @section('footer')
+<style type = "text/css">
+footer {text-align: center; padding: 10px; background: #ffffff;}
+</style>
 <footer class = "container-fluid">
 <small> Copyright © 2020 大熊崇仁</small>
 </footer>
